c94bc2f40d7786de9ffcddddd21a909099ecb623
John Larson <jl@buildmidwestern.com>
2023-07-23 06:08:54 -0500

    Adjusted temp demo layout

--- app/(site)/demo/page.tsx
+++ app/(site)/demo/page.tsx
@@ -1,9 +1,11 @@
 import FeaturedRecipes from '@/app/(site)/components/FeaturedRecipes';
+import CTA from '@/app/(site)/components/CTA';
 
 export default function Demo() {
   return (
     <main>
       <FeaturedRecipes />
+      <CTA />
     </main>
   );
 }
