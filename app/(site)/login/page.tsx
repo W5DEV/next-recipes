65c9c696512def3564188d1fcd40c77f94c9bbca
John Larson <jl@buildmidwestern.com>
2024-02-03 14:03:37 -0600

    feat: updated logo and login page

--- app/(site)/login/page.tsx
+++ app/(site)/login/page.tsx
@@ -1,13 +1,11 @@
+import Logo from '@/app/components/Logo';
+
 export default function Example() {
   return (
     <>
       <div className='flex flex-col justify-center flex-1 px-6 py-12 lg:px-8'>
-        <div className='sm:mx-auto sm:w-full sm:max-w-sm'>
-          <img
-            className='w-auto h-10 mx-auto'
-            src='https://tailwindui.com/img/logos/mark.svg?color=cyan&shade=600'
-            alt='Your Company'
-          />
+        <div className='flex flex-col items-center justify-center sm:mx-auto sm:w-full sm:max-w-sm'>
+          <Logo height='120' />
           <h2 className='mt-10 text-2xl font-bold leading-9 tracking-tight text-center text-gray-900'>
             Sign in to your account
           </h2>
