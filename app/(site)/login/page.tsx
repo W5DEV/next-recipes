f97efb21269be136a58f01a52071de1a58ee35b0
John Larson <jl@buildmidwestern.com>
2024-02-23 04:53:24 -0600

    chore: removed supabase dependency

--- app/(site)/login/page.tsx
+++ app/(site)/login/page.tsx
@@ -1,4 +1,3 @@
-/* eslint-disable @typescript-eslint/no-misused-promises */
 import Logo from '@/app/components/Logo';
 
 export default function Example() {
@@ -59,14 +58,12 @@ export default function Example() {
             <button
               type='submit'
               className='flex w-full justify-center rounded-md bg-cyan-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-cyan-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-cyan-600'
-              formAction={() => console.log('Sign in')}
             >
               Sign in
             </button>
             <button
               type='submit'
               className='flex w-full justify-center rounded-md bg-cyan-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-cyan-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-cyan-600'
-              formAction={() => console.log('Sign up')}
             >
               Sign up
             </button>
