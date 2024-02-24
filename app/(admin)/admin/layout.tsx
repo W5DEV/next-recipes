89e2fe3d521ae96858d9a87a9d20d386acde1cfb
John Larson <jl@buildmidwestern.com>
2024-02-23 19:52:10 -0600

    chore: updated data structure

--- app/(admin)/admin/layout.tsx
+++ app/(admin)/admin/layout.tsx
@@ -1,9 +1,6 @@
 import '../../globals.css';
 import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
 import Dashboard from '../../components/Dashboard';
-
-const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -13,7 +10,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang='en' className='h-full'>
-      <body className={inter.className}>
+      <body>
         <main>
           <Dashboard>{children}</Dashboard>
         </main>
