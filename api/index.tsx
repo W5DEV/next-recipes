ac8359dd0ab42d31f66da6cf4327fd242d7c4c06
John Larson <jl@buildmidwestern.com>
2024-02-24 09:59:45 -0600

    chore: fixed api

--- api/index.tsx
+++ api/index.tsx
@@ -4,7 +4,7 @@
 
 import axios from 'axios';
 
-const apiUrl = process.env.REACT_APP_API_URL;
+const apiUrl = process.env.API_URL;
 
 export const UserLogin = async (email: string, password: string): Promise<any> => {
   try {
--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
+  env: {
+    API_URL: process.env.API_URL,
+  },
   images: {
     remotePatterns: [
       {
